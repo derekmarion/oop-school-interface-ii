e3703177e6bd6a77f50f468408fcb620456dfe27
Mic-J-Lee <mleenavy@gmail.com>
2019-09-18 14:21:56 -0500

    Update person.py

--- classes/person.py
+++ classes/person.py
@@ -1,8 +1,6 @@
 class Person:
-
-  def __init__(self, name, age, password, role):
-    self.name = name
-    self.age = age
-    self.password = password
-    self.role = role
-      
+    def __init__(self, name, age, password, role):
+        self.name = name
+        self.age = age
+        self.password = password
+        self.role = role
