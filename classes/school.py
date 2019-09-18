8576444f6842668021b5f5c4ec14facbe66858ed
Mic-J-Lee <mleenavy@gmail.com>
2019-09-18 14:22:21 -0500

    Update school.py

--- classes/school.py
+++ classes/school.py
@@ -2,7 +2,7 @@ from classes.staff import Staff
 from classes.student import Student
 
 class School:
-  def __init__(self, name):
-    self.name = name
-    self.staff = Staff.objects()
-    self.students = Student.objects()
+    def __init__(self, name):
+        self.name = name
+        self.staff = Staff.objects()
+        self.students = Student.objects()
