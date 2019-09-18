d1240ab1975cb33299fc1c1e9dfd74cd6754c3d9
Mic-J-Lee <mleenavy@gmail.com>
2019-09-18 14:25:40 -0500

    Update student.py

--- classes/student.py
+++ classes/student.py
@@ -4,22 +4,19 @@ from classes.person import Person
 
 class Student(Person):
 
-  def __init__(self, name, age, password, role, school_id):
-    super().__init__(name, age, password, role)
-    self.school_id = school_id
+    def __init__(self, name, age, password, role, school_id):
+        super().__init__(name, age, password, role)
+        self.school_id = school_id
 
-  @classmethod
-  def objects(cls):
-    students = []
-    my_path = os.path.abspath(os.path.dirname(__file__))
-    path = os.path.join(my_path, "../data/students.csv")
+    @classmethod
+    def objects(cls):
+        students = []
+        my_path = os.path.abspath(os.path.dirname(__file__))
+        path = os.path.join(my_path, "../data/students.csv")
     
-    with open(path) as csvfile:
-      reader = csv.DictReader(csvfile)
-      for row in reader:
-        students.append(Student(**dict(row)))
+        with open(path) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                students.append(Student(**dict(row)))
     
-    return students
-
-
-
+        return students
