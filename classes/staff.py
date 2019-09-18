94bb31c98726276cdb9cdd6d2e9c253a1f92d960
Mic-J-Lee <mleenavy@gmail.com>
2019-09-18 14:24:25 -0500

    Update staff.py

--- classes/staff.py
+++ classes/staff.py
@@ -4,20 +4,19 @@ from classes.person import Person
 
 class Staff(Person):
   
-  def __init__(self, name, age, password, role, employee_id):
-    super().__init__(name, age, password, role)
-    self.employee_id = employee_id
+    def __init__(self, name, age, password, role, employee_id):
+        super().__init__(name, age, password, role)
+        self.employee_id = employee_id
 
-  @classmethod
-  def objects(cls):
-    staff = []
-    my_path = os.path.abspath(os.path.dirname(__file__))
-    path = os.path.join(my_path, "../data/staff.csv")
+    @classmethod
+    def objects(cls):
+        staff = []
+        my_path = os.path.abspath(os.path.dirname(__file__))
+        path = os.path.join(my_path, "../data/staff.csv")
 
-    with open(path) as csvfile:
-      reader = csv.DictReader(csvfile)
-      for row in reader:
-        staff.append(Staff(**dict(row)))
+        with open(path) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                staff.append(Staff(**dict(row)))
 
-    return staff
-
+        return staff
