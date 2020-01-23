d804eeaf23af0f54627936247d14a0f5ea20cf38
Tom <prete316@gmail.com>
2020-01-23 10:06:46 -0600

    Update school.py

--- classes/school.py
+++ classes/school.py
@@ -6,15 +6,3 @@ class School:
         self.name = name
         self.staff = Staff.objects()
         self.students = Student.objects()
-
-    def list_students(self):
-        counter = 1
-        for student in (self.students):
-            print(f"{counter}. {student.name} {student.school_id}")
-            counter += 1
-
-    def find_student_by_id(self, student_id):
-        for student in (self.students):
-            if student_id == student.school_id:
-                return student
-        return "\nStudent not found."
